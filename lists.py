2f6b6d7738bc96203b69840cde76e2c96728c1f2
Shehzab <sinanshehzab@gmail.com>
2025-12-09 12:11:26 +0530

    wordGuess game ✅

--- lists.py
+++ lists.py
@@ -1,9 +1,49 @@
 numbers = [1,2,3,4,5]
 
-print(len(numbers))
+# print(len(numbers))
 
 # mixed type of lists
 
 mix = [1, "hello" , 3.12 ]
-print(mix[1])
-  
+# print(mix[1])
+
+
+# Accessing Items
+
+my_list = [1,2,3,4,5]
+# print(my_list[-2])
+
+
+
+# using slices
+
+my_list_slice = [0,1,2,3,4,5,6,7,8,9]
+subset = my_list_slice[1:9:2]
+subset1 = my_list_slice[4]
+subset3 = my_list_slice[:6]
+# print(subset3)
+# print(subset)
+# print(subset1)
+ 
+
+#updating items
+
+one = [1,2,3,4,5,6,7,8,9,0]
+
+one[2] = 12
+
+print(one)
+
+#removing using del
+
+two = [1,2,3,4,5]
+
+del two[1] 
+print(two)
+
+
+#removing using remove()
+
+five = [1,2,3,4]
+five.remove(3)
+print(five)
---
+++ word_guess.py
@@ -0,0 +1,28 @@
+import random 
+
+def getRandomNumber():
+  return random.randrange(1,100)
+
+def giveHint(number, guess):
+  if guess > (number + 10) or guess < (number - 10):
+    return "cold"
+  elif number == guess:
+    return "right"
+  else:
+    return "Hot"
+  
+def runGuess():
+    secretNumber = getRandomNumber()
+
+    while True:
+      user_guess = int(input("Enter a number between 1 and 100"))
+      hint = giveHint(secretNumber, user_guess)
+
+      if hint == "right":
+        print("You guessed it Right!!")
+        break
+      else:
+        print(hint)
+
+if __name__ == '__main__':
+  runGuess()
